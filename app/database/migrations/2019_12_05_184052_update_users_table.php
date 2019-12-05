396f9c700309772b845f0235ed8bcb8e3c24fbc2
Shasi Kanth Dulipalla <shasi.dulipalla@valmont.com>
2019-12-06 00:24:45 +0530

    changes

---
+++ app/database/migrations/2019_12_05_184052_update_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table)
+		{
+			$table->renameColumn('role_id', 'role');
+			$table->boolean('is_active')->after('updated_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+		});
+	}
+
+}
